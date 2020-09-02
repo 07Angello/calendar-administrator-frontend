c43810b18e67f1ed95b82665a72e8e4b3a9c5f2b
Angello Antonelly <angelloantonelly@live.com>
2020-09-01 21:53:02 -0600

    created new register screen reusing custom auth classes

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,31 +3,29 @@ import './custom-style.css';
 
 export const LoginScreen = () => {
     return (
-
-            <div className="custom-container">
-                <div className="col-md-4 custom-form">
-                    <div className="img-container">
-                        <img src={ require('../../assets/user.svg') } className='user-img' alt="User icon"/>
-                    </div>
-                    <div className="form-container">
-                        <h3>Calendar Events</h3>
-                        <form>
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="email" />
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" placeholder="password" />
-                            </div>
-                            <div className="form-group">
-                                <input type="submit" className="btnSubmit" value="Login" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="form-footer">
-                        <a className="underlineHover">Register</a>
-                    </div>
+        <div className="custom-container">
+            <div className="col-md-4 custom-form">
+                <div className="img-container">
+                    <img src={ require('../../assets/user.svg') } className='user-img' alt="User icon"/>
+                </div>
+                <div className="form-container">
+                    <h3>Calendar Events</h3>
+                    <form>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="email" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="password" />
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" className="btnSubmit" value="Login" />
+                        </div>
+                    </form>
+                </div>
+                <div className="form-footer">
+                    <a className="underlineHover">Register</a>
                 </div>
             </div>
-
+        </div>
     )
 }
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './custom-style.css';
 
 export const RegisterScreen = () =>{
     return (
-        <h1>Register</h1>
+        <div className="custom-container">
+            <div className="col-md-4 custom-form">
+                <div className="form-container">
+                    <h3>Register</h3>
+                    <form>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Name" />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Email" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Password" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Confirm Password" />
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" className="btnSubmit" value="Register" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
