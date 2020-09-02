76aaa4074417e611abadebc205174677c3927bb4
Angello Antonelly <angelloantonelly@live.com>
2020-09-02 12:24:39 -0600

    added react-big-calendar component and created out main navbar

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { AppRouter } from './routers/AppRouter';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { Navbar } from './components/ui/Navbar';
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'Cumpleaños de la flaquis',
+    start: moment().toDate(),
+    end: moment().add( 2, 'hours' ).toDate(),
+    bgcolor: '#fafafa'
+}]
 
 export const CalendarApp = () => {
     return (
-        <AppRouter />
+        <div className="calendar-screen">
+            <Navbar />
+
+            <Calendar
+                localizer={ localizer }
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+            />
+
+        </div>
     )
 }
