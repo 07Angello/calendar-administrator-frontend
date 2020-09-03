9d367e4ed6664423865091111401188583d5b258
Angello Antonelly <angelloantonelly@live.com>
2020-09-02 19:57:22 -0600

    Added custom style to react-modal component

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top : '50%',
+        left : '50%',
+        right : 'auto',
+        bottom : 'auto',
+        marginRight : '-50%',
+        transform : 'translate(-50%, -50%)'
+    }
+}
+
+Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
+
+    const [ isOpen, setIsOpen ] =  useState(true);
+
+    const closeModal = () => {
+        setIsOpen( false );
+    }
+
     return (
         <div>
-            Modal Test
+            <Modal
+                isOpen={ isOpen }
+                // onAfterOpen={ afteropenModal }
+                onRequestClose={ closeModal }
+                style={ customStyles }
+                closeTimeoutMS={ 200 }
+                className="modal"
+                overlayClassName="modal-background"
+            >
+                <h1>Modal Event</h1>
+                <hr/>
+                <span> MODAL </span>
+            </Modal>
         </div>
     )
 }
