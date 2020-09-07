11976cec39103da5ac2051154237cafe491d500c
Angello Antonelly <angelloantonelly@live.com>
2020-09-06 21:10:40 -0600

    Added renew token functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { fetchWithoutToken } from "../helpers/fetch";
+import { fetchWithoutToken, fetchWithToken } from "../helpers/fetch";
 import { types } from '../types/types';
 
 import { toast } from 'react-toastify';
@@ -43,6 +43,25 @@ export const registerUser = ( name, email, password ) => {
             }) );
 
             toast.success( `Hello ${ Data.name } enjoy Calendar App!` );
+        }
+    }
+}
+
+export const startChecking = () => {
+    return async( dispatch ) => {
+        const response = await fetchWithToken( 'auth/renew-token');
+        const { Data, Token } = await response.json();
+
+        if ( Token && Token.length > 0 ) {
+            localStorage.setItem('token', Token);
+            localStorage.setItem('token-init-date', new Date().getTime() );
+
+            dispatch( login({
+                uid: Data._id,
+                name: Data.name
+            }) );
+        } else {
+            toast.error('Could not generate a nuew token.');
         }
     }
 }
--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,6 +1,6 @@
 const baseUrl = process.env.REACT_APP_API_URL;
 
-export const fetchWithoutToken = ( endpoint, data, method = 'GET' ) => {
+const fetchWithoutToken = ( endpoint, data, method = 'GET' ) => {
 
     const url = `${ baseUrl }/${ endpoint }`; // localhost://4000/api/events/
 
@@ -21,19 +21,35 @@ export const fetchWithoutToken = ( endpoint, data, method = 'GET' ) => {
     }
 }
 
-export const fetchWithToken = ( endpoint, data, method = 'GET') => {
+const fetchWithToken = ( endpoint, data, method = 'GET') => {
 
     const url = `${ baseUrl }/${ endpoint }`;
+    const token = localStorage.getItem('token') || '';
 
     if ( method === 'GET' ) {
-        return fetch( url );
+
+        return fetch( url,{
+            method,
+            headers: {
+                'x-token': token
+            }
+        } );
+
     } else {
+
         return fetch( url, {
             method,
             headers: {
-                'Content-type': 'application/json'
+                'Content-type': 'application/json',
+                'x-token': token
             },
-            body: JSON.stringify( data );
+            body: JSON.stringify( data )
         } );
+
     }
+}
+
+export {
+    fetchWithoutToken,
+    fetchWithToken
 }
